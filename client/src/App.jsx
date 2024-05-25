ad1d689bc151294ac925962e8a4d8e292d6165a0
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-05-26 00:32:58 +0530

    Add PlaceItem, PlaceList, and UserPlaces

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,15 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import RootLayout from "./RootLayout";
+import UserPlaces from "./places/pages/UserPlaces";
 
 const router = createBrowserRouter([
   {
     path: '/', element: <RootLayout />,
     children: [
       { path: "/", element: <Users /> },
+      { path: "/:userId/places", element: <UserPlaces /> },
       { path: "/places/new", element: <NewPlace /> },
       { path: "*", element: <Navigate to="/" replace /> } 
     ]
