0ee320c895d51b6c3b65b408645304d3e3760707
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-05-26 17:34:03 +0530

    Add Modal and Modal Overlay

--- client/src/places/components/PlaceItem.jsx
+++ client/src/places/components/PlaceItem.jsx
@@ -1,7 +1,8 @@
+import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import './PlaceItem.css';
 
-const PlaceItem = ({ image, title, address, description }) => {
+const PlaceItem = ({ id, image, title, address, description }) => {
   return (
     <li className="place-item">
       <Card className="place-item__content">
@@ -14,9 +15,9 @@ const PlaceItem = ({ image, title, address, description }) => {
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
