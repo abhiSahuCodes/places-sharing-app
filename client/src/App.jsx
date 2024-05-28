d79f72ab5ea33e892b1e4b6d2381fc009edcd59a
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-05-28 15:09:21 +0530

    Add Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,25 +8,25 @@ import NewPlace from "./places/pages/NewPlace";
 import RootLayout from "./RootLayout";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 const router = createBrowserRouter([
   {
-    path: '/', element: <RootLayout />,
+    path: "/",
+    element: <RootLayout />,
     children: [
       { path: "/", element: <Users /> },
       { path: "/:userId/places", element: <UserPlaces /> },
       { path: "/places/new", element: <NewPlace /> },
-      {path: "/places/:placeId", element: <UpdatePlace />},
-      { path: "*", element: <Navigate to="/" replace /> } 
-    ]
-  }
-,
+      { path: "/places/:placeId", element: <UpdatePlace /> },
+      { path: "/auth", element: <Auth /> },
+      { path: "*", element: <Navigate to="/" replace /> },
+    ],
+  },
 ]);
 
 const App = () => {
-  return (
-      <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
