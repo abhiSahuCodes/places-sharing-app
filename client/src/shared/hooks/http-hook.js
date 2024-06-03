c22f880b24108e272877fa59617e5a96075686f7
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-06-03 23:01:57 +0530

    Add http-hook: custom hook for api fetch

---
+++ client/src/shared/hooks/http-hook.js
@@ -0,0 +1,57 @@
+import { useCallback, useState, useEffect } from "react";
+
+const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (error) {
+        setError(error.message || "Something went wrong!!");
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    clearError,
+    sendRequest,
+  };
+};
+
+export default useHttpClient;
