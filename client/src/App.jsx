9d94d6a8be74a9b7ff2c8a549ce76ab6ca4172ea
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-06-06 00:39:10 +0530

    Add token in localStorage for some frontend tweeking/checking(though not secure like HTTP cookies)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,10 +10,41 @@ import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context.jsx";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 const App = () => {
   const auth = useContext(AuthContext);
+
+  let logoutTimer = useRef();
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      auth.login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [auth.login]);
+
+  useEffect(() => {
+    if (auth.token && auth.tokenExpirationDate) {
+      const remainingTime =
+        auth.tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer.current = setTimeout(auth.logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer.current);
+    }
+
+    return() => {
+      clearTimeout(logoutTimer.current);
+    }
+  }, [auth.token, auth.logout, auth.tokenExpirationDate]);
 
   let routes = [
     { path: "/", element: <Users /> },
--- client/src/shared/context/auth-context.jsx
+++ client/src/shared/context/auth-context.jsx
@@ -11,19 +11,33 @@ export const AuthContext = createContext({
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
     setUserId(uid);
   }, []);
+
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: !!token, token, userId, login, logout }}
+      value={{ isLoggedIn: !!token, token, userId, tokenExpirationDate, login, logout }}
     >
       {children}
     </AuthContext.Provider>
